ff30812810e0f8822392755aa906e41f57e8150a
muhammadqamar <muhammadqamar111@gmail.com>
2023-07-12 14:59:47 +0500

    adding targetcustomer component

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,15 +2,17 @@ import React from "react"
 import Hero from "./hero"
 import Trusted from "./trusted"
 import Audience from "./audience"
+import TargetCustomers from "./targetCustomers"
 
 const Home = () => {
   return (
     <>
       <Hero />
       <div className="w-full  px-[20px] md:px-[clamp(20px,12vw,122px)] pt-[64px]">
-        <div className="max-w-[1084px] mx-auto">
+        <div className="mx-auto">
           <Trusted />
           <Audience />
+          <TargetCustomers/>
         </div>
       </div>
     </>
