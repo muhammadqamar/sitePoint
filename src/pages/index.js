a68de1ae8d213fda05bca940925377dbf89a8d19
muhammadqamar <muhammadqamar111@gmail.com>
2023-10-19 12:57:01 +0500

    add react-slick slider

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import Seo from "../components/layout/seo"
 import Layout from "../components/layout"
 import Home from "../components/Home/home"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 const IndexPage = () => (
   <Layout>
